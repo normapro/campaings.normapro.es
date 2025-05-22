d21d1e50317fff147a2d97dfdf3f37ae5f2626c1
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-05-22 16:29:08 +0200

    Tercera sección de la página creada, e incluida en el layout.

---
+++ src/components/ReunionPersonalizada.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+const ReunionPersonalizada = () => {
+    return (
+        <div>
+            <div className="flex">
+                <div>
+                    <h2>Confianza y</h2>
+                    <h2>credibilidad</h2>
+
+                    <div>
+                        <p>Este test ha sido elaborado por expertos</p>
+                        <p>consultores del Instituto de Innovación, Ciencia</p>
+                        <p>y Empresa, que llevan más de veinte años</p>
+                        <p>dedicados a la implantación de sistemas de</p>
+                        <p>gestión y cumplimiento normativo en</p>
+                        <p>organizaciones de todos los tamaños y</p>
+                        <p>sectores.</p>
+                    </div>
+
+                    <div>
+                        <p>Más que una herramienta, es el punto de</p>
+                        <p>partida para visualizar con claridad dónde</p>
+                        <p>estás y hacia dónde puedes avanzar.</p>
+                    </div>
+
+                </div>
+                <div>
+                    <h2>Un test.</h2>
+                    <h2>Un diagnóstico</h2>
+                    <h2>Un nuevo enfoque.</h2>
+
+                    <div>
+                        <p>Empieza por medir. Después, podrás decidir</p>
+                        <p>cómo avanzar.</p>
+                    </div>
+
+                    <div>
+                        <p>Este test te ofrece claridad, dirección y </p>
+                        <p>enfoque. Y si descubres que hay margen de</p>
+                        <p>mejora, en Norma Pro estamos listos para</p>
+                        <p>acompañarte con soluciones digitales que</p>
+                        <p>convierten el cumplimiento en una palanca</p>
+                        <p>real de eficiencia y crecimiento.</p>
+                    </div>
+
+                </div>
+                <div>
+                    <h2>¿Quieres avanzar con</h2>
+                    <h2>acompañamiento</h2>
+                    <h2>experto?</h2>
+
+                    <p>Agenda una reunión con nuestro equipo y te</p>
+                    <p>contaremos cómo ayudarte a interpretar tus</p>
+                    <p>resultados, priorizar acciones y avanzar hacia</p>
+                    <p>un modelo de cumplimiento más eficiente,</p>
+                    <p>trazable y alineado con tu realidad empresarial.</p>
+                </div>
+            </div>
+            <div>
+                <button>Solicita una reunión personalizada</button>
+            </div>
+        </div>
+    );
+};
+
+export default ReunionPersonalizada;
