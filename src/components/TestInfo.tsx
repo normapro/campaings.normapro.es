8e40aea3546e677c1cb67d126e296139b13ed86c
JuanAlberto27 <juanjurado.external@institutodeinnovacion.es>
2025-05-22 13:19:26 +0200

    update

--- src/components/TestInfo.tsx
+++ src/components/TestInfo.tsx
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import Image from 'next/image';
-import image1 from '../../public/imagen1.svg';
-import image2 from '../../public/imagen2.svg';
-import image3 from '../../public/imagen3.png';
+import image1 from '../../public/imagenSeccion21.svg';
+import image2 from '../../public/imagenSeccion32.svg';
+import image3 from '../../public/imagenSeccion23.png';
 
 const TestInfo = () => {
   return (
