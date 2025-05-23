c734906688a0cfc70be9ed844f1a9474a91374d0
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-05-23 11:28:36 +0200

    Estilos añadidos y otros modificados. Sección 3 mejor integrada y cuadrada a la página.

--- src/components/ReunionPersonalizada.tsx
+++ src/components/ReunionPersonalizada.tsx
@@ -2,105 +2,118 @@
 
 const ReunionPersonalizada = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center px-4 mb-[-170px] mt-[120px]">
+        <div className="min-h-screen flex flex-col items-center justify-center px-4 mb-[-60px] mt-[10px]">
             <div className="flex justify-center gap-x-12 text-left max-w-7xl">
-                <div className="max-w-sm border-r border-[#cccfd8] pr-[40px]">
+                <div className="max-w-sm border-r border-[#cccfd8] pr-[40px] h-full min-h-[600px]">
 
-                    <img
-                        src="/IICE_LandingCompliancePenal_Nivel2_1.svg"
-                        alt="Decoración"
-                        className="absolute top-[1940px] left-[374px] w-[264px] h-[253px] pointer-events-none select-none"
-                    />
+                    <div className="mt-[205px]">
+                        <img
+                            src="/IICE_LandingCompliancePenal_Nivel2_1.svg"
+                            alt="Decoración"
+                            className="absolute top-[1940px] left-[374px] w-[264px] h-[253px] pointer-events-none select-none"
+                        />
 
-                    <img
-                        src="/Grupo 4580.png"
-                        alt="Decoración"
-                        className="absolute top-[1940px] left-[374px] w-[264px] h-[253px] pointer-events-none select-none"
-                    />
+                        <img
+                            src="/Grupo 4580.png"
+                            alt="Decoración"
+                            className="absolute top-[1940px] left-[374px] w-[264px] h-[253px] pointer-events-none select-none"
+                        />
 
-                    <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">Confianza y</h2>
-                    <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">credibilidad</h2>
+                        <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">Confianza y</h2>
+                        <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">credibilidad</h2>
 
-                    <div className="mt-[30px] text-[#010D3D] mt-4">
-                        <p>Este test ha sido elaborado por expertos</p>
-                        <p>consultores del Instituto de Innovación, Ciencia</p>
-                        <p>y Empresa, que llevan más de veinte años</p>
-                        <p>dedicados a la implantación de sistemas de</p>
-                        <p>gestión y cumplimiento normativo en</p>
-                        <p>organizaciones de todos los tamaños y</p>
-                        <p>sectores.</p>
-                    </div>
+                        <div className="mt-[30px] text-[#010D3D] mt-4">
+                            <p>Este test ha sido elaborado por expertos</p>
+                            <p>consultores del Instituto de Innovación, Ciencia</p>
+                            <p>y Empresa, que llevan más de veinte años</p>
+                            <p>dedicados a la implantación de sistemas de</p>
+                            <p>gestión y cumplimiento normativo en</p>
+                            <p>organizaciones de todos los tamaños y</p>
+                            <p>sectores.</p>
+                        </div>
 
-                    <div className="text-[#010D3D] mt-4">
-                        <p>Más que una herramienta, es el punto de</p>
-                        <p>partida para visualizar con claridad dónde</p>
-                        <p>estás y hacia dónde puedes avanzar.</p>
+                        <div className="text-[#010D3D] mt-4">
+                            <p>Más que una herramienta, es el punto de</p>
+                            <p>partida para visualizar con claridad dónde</p>
+                            <p>estás y hacia dónde puedes avanzar.</p>
+                        </div>
+
                     </div>
                 </div>
 
                 <div className="max-w-sm">
 
-                    <img
-                        src="/IICE_LandingCompliancePenal_Nivel1_6.svg"
-                        alt="Decoración"
-                        className="absolute top-[1986px] left-[830px] w-[218px] h-[191px] pointer-events-none select-none"
-                    />
+                    <div className="mt-[205px]">
 
-                    <img
-                        src="/Grupo 5276.png"
-                        alt="Decoración"
-                        className="absolute top-[1966px] left-[850px] w-[218px] h-[191px] pointer-events-none select-none"
-                    />
+                        <img
+                            src="/IICE_LandingCompliancePenal_Nivel1_6.svg"
+                            alt="Decoración"
+                            className="absolute top-[1986px] left-[830px] w-[218px] h-[191px] pointer-events-none select-none"
+                        />
 
-                    <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">Un test.</h2>
-                    <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">Un diagnóstico</h2>
-                    <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">Un nuevo enfoque.</h2>
+                        <img
+                            src="/Grupo 5276.png"
+                            alt="Decoración"
+                            className="absolute top-[1966px] left-[850px] w-[218px] h-[191px] pointer-events-none select-none"
+                        />
 
-                    <div className="mt-[30px] text-[#010D3D] mt-4">
-                        <p>Empieza por medir. Después, podrás decidir</p>
-                        <p>cómo avanzar.</p>
+                        <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">Un test.</h2>
+                        <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">Un diagnóstico</h2>
+                        <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">Un nuevo enfoque.</h2>
+
+                        <div className="mt-[30px] text-[#010D3D] mt-4">
+                            <p>Empieza por medir. Después, podrás decidir</p>
+                            <p>cómo avanzar.</p>
+                        </div>
+
+                        <div className="text-[#010D3D] mt-4">
+                            <p>Este test te ofrece claridad, dirección y</p>
+                            <p>enfoque. Y si descubres que hay margen de</p>
+                            <p>mejora, en Norma Pro estamos listos para</p>
+                            <p>acompañarte con soluciones digitales que</p>
+                            <p>convierten el cumplimiento en una palanca</p>
+                            <p>real de eficiencia y crecimiento.</p>
+                        </div>
+
                     </div>
 
-                    <div className="text-[#010D3D] mt-4">
-                        <p>Este test te ofrece claridad, dirección y</p>
-                        <p>enfoque. Y si descubres que hay margen de</p>
-                        <p>mejora, en Norma Pro estamos listos para</p>
-                        <p>acompañarte con soluciones digitales que</p>
-                        <p>convierten el cumplimiento en una palanca</p>
-                        <p>real de eficiencia y crecimiento.</p>
-                    </div>
                 </div>
 
                 <div className="max-w-sm border-l border-[#cccfd8] pl-[40px]">
 
-                    <img
-                        src="/IICE_LandingCompliancePenal_Nivel2_3.svg"
-                        alt="Decoración"
-                        className="absolute top-[1938px] left-[1237px] w-[285px] h-[249px] pointer-events-none select-none"
-                    />
+                    <div className="mt-[205px]">
 
-                    <img
-                        src="/Grupo 5265.png"
-                        alt="Decoración"
-                        className="absolute top-[1938px] left-[1237px] w-[285px] h-[249px] pointer-events-none select-none"
-                    />
+                        <img
+                            src="/IICE_LandingCompliancePenal_Nivel2_3.svg"
+                            alt="Decoración"
+                            className="absolute top-[1938px] left-[1237px] w-[285px] h-[249px] pointer-events-none select-none"
+                        />
 
-                    <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">¿Quieres avanzar con</h2>
-                    <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">acompañamiento</h2>
-                    <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">experto?</h2>
+                        <img
+                            src="/Grupo 5265.png"
+                            alt="Decoración"
+                            className="absolute top-[1938px] left-[1237px] w-[285px] h-[249px] pointer-events-none select-none"
+                        />
 
-                    <div className="mt-[30px] text-[#010D3D] mt-4">
-                        <p>Agenda una reunión con nuestro equipo y te</p>
-                        <p>contaremos cómo ayudarte a interpretar tus</p>
-                        <p>resultados, priorizar acciones y avanzar hacia</p>
-                        <p>un modelo de cumplimiento más eficiente,</p>
-                        <p>trazable y alineado con tu realidad empresarial.</p>
+                        <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">¿Quieres avanzar con</h2>
+                        <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">acompañamiento</h2>
+                        <h2 className="text-[#010D3D] font-extrabold text-[28px] leading-tight">experto?</h2>
+
+                        <div className="mt-[30px] text-[#010D3D] mt-4">
+                            <p>Agenda una reunión con nuestro equipo y te</p>
+                            <p>contaremos cómo ayudarte a interpretar tus</p>
+                            <p>resultados, priorizar acciones y avanzar hacia</p>
+                            <p>un modelo de cumplimiento más eficiente,</p>
+                            <p>trazable y alineado con tu realidad empresarial.</p>
+                        </div>
+
                     </div>
+
                 </div>
             </div>
 
             <div className="mt-12">
-                <button className="hover:bg-[#010D3D] bg-gradient-to-r from-[#00B8EC] to-[#D6A4F1] text-white px-6 py-3 rounded-lg font-semibold border-none focus:outline-none">
+                <button className="hover:bg-[#2c365e] bg-gradient-to-r from-[#00B8EC] to-[#D6A4F1] text-white px-6 py-3 rounded-lg font-semibold border-none focus:outline-none">
                     Solicita una reunión personalizada
                 </button>
             </div>
